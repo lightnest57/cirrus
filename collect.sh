61d1345730364b403614a33ccef2d997bc62b671
R.A.P <rhmd.19801@gmail.com>
2022-09-01 11:30:31 +0000

    ccache arcana
    Signed-off-by: R.A.P <rhmd.19801@gmail.com>

---
+++ collect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ~/rom
+
+. build/envsetup.sh
+export CCACHE_DIR=~/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G -F 0
+$lunch
+$make -j8 &
+sleep 100m
+kill %1
+ccache -s
---
+++ final.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ~/rom
+
+. build/envsetup.sh
+export CCACHE_DIR=~/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G -F 0
+ccache -o compression=true
+ccache -z
+
+$lunch
+$make -j8
