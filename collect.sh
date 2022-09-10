1eb27c44b2e0326c9b44046429c2f74fb3fba022
darknius22 <112658401+darknius22@users.noreply.github.com>
2022-09-09 21:59:48 -0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -7,6 +7,9 @@ export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G -F 0
 $lunch
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
+export BUILD_BROKEN_DUP_RULES=true
 $make -j8 &
 sleep 100m
 kill %1
