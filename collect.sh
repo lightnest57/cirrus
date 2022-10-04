eabe5467920493c137b90c9ac1d46a3632fd6d96
darknight <darkniuslightness@gmail.com>
2022-10-03 19:30:47 -0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -7,6 +7,7 @@ export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 ccache -M 20G -F 0
 $lunch
+export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
