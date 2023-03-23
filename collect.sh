ee770dd9bc1a0ecdaa41113c5e88b6b7de93e8ad
lightnest57 <lightnest57@gmail.com>
2023-03-23 11:15:29 +0000

    HavocOS ccache

--- collect.sh
+++ collect.sh
@@ -4,7 +4,6 @@ cd ~/rom
 . build/envsetup.sh
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
-export USE_CCACHE=1
 ccache -M 20G -F 0
 export BUILD_USERNAME="darknius"
 export BUILD_HOSTNAME="darx-labs"
