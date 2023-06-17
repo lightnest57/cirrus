caa9ce8c9bff3a47fc77f2beeeebf5bca5b7d7d8
lightnest57 <lightnest57@gmail.com>
2023-06-17 05:44:58 +0000

    topaz

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build..."
-rclone copy ~/rom/out/target/product/citrus/PixelDust-*.zip fm293:citrus -P
+rclone copy ~/rom/out/target/product/citrus/aospa-topaz*.zip fm293:citrus -P
 curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls *.zip) Uploaded Successfully!"
