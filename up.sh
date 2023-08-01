d6b14aa328a6a7ddaf433436bc569dc50e450924
lightnest57 <lightnest57@gmail.com>
2023-08-01 09:57:16 +0000

    nad

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
-curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls *.zip)"
-rclone copy ~/rom/out/target/product/juice/xdCLO_*.zip 298:juice -P
-curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls *.zip) Uploaded Successfully!"
+curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls *.zip)"
+rclone copy ~/rom/out/target/product/citrus/Nusantara-v*.zip 298:citrus -P
+curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls *.zip) Uploaded Successfully!"
