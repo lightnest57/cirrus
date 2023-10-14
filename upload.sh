99e4105c4a00496430576ac79fdb9d5a46d39574
lightnest57 <122135694+lightnest57@users.noreply.github.com>
2023-10-14 23:46:52 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/juice/Havoc-OS*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls *.zip)"
+if [ -f ~/rom/out/target/product/citrus/Havoc-OS*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls *.zip)"
       rclone copy ~/rom/out/target/product/citrus/Havoc-OS*.zip 298:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls *.zip) Uploaded Successfully!"
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls *.zip) Uploaded Successfully!"
 fi
